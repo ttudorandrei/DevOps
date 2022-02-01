f782a6ad9536b215c82af7563abb24b0e6aaf420
Tudor Tocan <tudor.tocan@gmail.com>
2022-02-01 16:20:54 +0000

    automate installing dependencies to vm in provision file

--- src/provision.sh
+++ src/provision.sh
@@ -8,3 +8,15 @@ sudo apt-get upgrade -y
 
 # install nginx
 sudo apt-get install nginx -y
+
+# download specific nodejs version with curl
+curl -sl https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
+# install previously downloaded nodejs
+sudo apt-get install nodejs -y
+
+# install pm2 package globally
+sudo npm install pm2 -g
+
+# change directory to specified path
+# cd /app/app
